4fa03812790eed7b12fd0a62f9b13b010bd97fe7
Fin <finlay@mckenziecorp.com.au>
2022-05-17 13:28:52 +1000

    image uploads

--- create_post.php
+++ create_post.php
@@ -6,9 +6,10 @@
     $newTitle = htmlspecialchars($_POST["post-title"]);
     $newAuthor = $_SESSION["username"];
     $newPost = htmlspecialchars($_POST["post-body"]);
+    $newImage = $_POST["post-image"];
     
-    $command = "INSERT INTO blogPost (username, title, post_body)
-    VALUES ('$newAuthor', '$newTitle', '$newPost');";
+    $command = "INSERT INTO blogPost (username, title, post_body, post_image)
+    VALUES ('$newAuthor', '$newTitle', '$newPost', '$newImage');";
 
     if ($conn->query($command) === TRUE) {
         header('Location: index.php');
