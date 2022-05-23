9b43e1181f80a53558c7f8eccf69a975a740494c
jofbob <jofbob@hotmail.com>
2022-05-23 21:37:41 +1000

    start creating comments

--- blogpost.php
+++ blogpost.php
@@ -1,5 +1,29 @@
 <?php
 session_start();
+require "dbconn.php";
+
+$title;
+
+//If there is a post id in get, and the user is not a visitor, they can use this page
+if(isset($_GET["post_id"]) && isset($_SESSION["role"]) && $_SESSION["role"] != "Visitor") {
+
+    $id = $_GET["post_id"];
+
+    $query = "SELECT title, archived, post_id from blogPost WHERE post_id = '$id' AND archived='0';";
+
+    $result = $conn->query($query);
+
+    if($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        $title = $row["title"];
+    } else {
+       header('Location: index.php');
+    }
+} else {
+    header('Location: index.php');
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +32,7 @@ session_start();
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home</title>
+    <title><?php echo $title; ?></title>
     <!-- Font load -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -27,45 +51,48 @@ session_start();
     <!-- Blog posts in reverse chronological order -->
     <?php
     require "navbar.php";
-    require "dbconn.php";
     ?>
     <main class="blog-posts">
         <?php
-
-        // //hide create post button if role != author
-        if (isset($_SESSION["role"]) && $_SESSION["role"] == "Author") {
-            echo "<button class=\"create-post-button\" onclick=\"window.location.href = 'create.php';\">";
-            echo "<img class=\"create-post-icon\" src=\"images/pen-paper-icon.png\" />Create Post";
-            echo "</button>";
-        }
-
-        //Select blog details where they aren't archived,
-        //format date to look nice
         $query = "SELECT username,
         title, 
         post_body, 
         post_image,
         archived, 
         DATE_FORMAT(post_date, \"%d %M %Y\") AS 'date'
-        from blogPost WHERE archived = \"0\" 
-        ORDER BY post_date DESC;";
-        
+        from blogPost WHERE post_id = '$id';";
         $result = $conn->query($query);
 
-        if ($result && $result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
+        if($result && $result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+
+            echo "<article class=\"blog-post\">";
+            echo "<div class=\"blog-post-text\">";
+            echo "<h2>" . $row["title"] . "</h2>";
+            echo "<h3 style=\"font-weight: 300; \">" . $row["username"] . "</h3>";
+            echo "<h3>" . $row["date"] . "</h3>";
+            echo "<p>" . $row["post_body"] . "</p>";
+            echo "</div>";
+            echo "<img class=\"blog-post-image\" src=\"" . $row["post_image"] . "\">";
+            echo "</article>";
+
+            $query = "SELECT post_id, comment_body, username, DATE_FORMAT(date, \"%d %M %Y\") AS 'dob' from Comment WHERE post_id = '$id'";
+
+            $result = $conn->query($query);
+
+            if($result && $result->num_rows > 0) {
                 echo "<article class=\"blog-post\">";
                 echo "<div class=\"blog-post-text\">";
-                echo "<h2>" . $row["title"] . "</h2>";
                 echo "<h3 style=\"font-weight: 300; \">" . $row["username"] . "</h3>";
                 echo "<h3>" . $row["date"] . "</h3>";
                 echo "<p>" . $row["post_body"] . "</p>";
-                //comments button
-                //echo "<button style=\"margin: 1rem 0 0 0; padding: 0.5rem;\">Comments</button>";
                 echo "</div>";
-                echo "<img class=\"blog-post-image\" src=\"" . $row["post_image"] . "\">";
                 echo "</article>";
             }
+
+        } else {
+            echo "Something went wrong here!";
+            //header('Location: index.php');
         }
         ?>
     </main>
